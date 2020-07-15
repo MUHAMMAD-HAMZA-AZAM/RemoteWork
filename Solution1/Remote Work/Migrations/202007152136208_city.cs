5b9fe49e35c03d5148d68b6b4e129327aa02bbc5
uzair2299 <uzairanwar2299@gmail.com>
2020-07-16 02:38:32 +0500

    New City

---
+++ Solution1/Remote Work/Migrations/202007152136208_city.cs
@@ -0,0 +1,26 @@
+﻿namespace Remote_Work.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class city : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cities",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CityName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Cities");
+        }
+    }
+}
